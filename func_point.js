ef3f08c662438500bb61eff3bf40dcfcca0c9f19
Kevin <nait90@gmail.com>
2024-09-29 16:57:37 +0900

    Update func_point.js

--- func_point.js
+++ func_point.js
@@ -1,39 +1,46 @@
-
-
 const input1 = ['카드', '포인트', '보험']
 const fs = require('fs')
 
 const content = fs.readFileSync('./kwd.input', 'utf8')
-const input = content.split(/[\s,]+/)
+const input = content.split(/[,\r\n]+/)
 console.log('input : ', input)
 
 function getAction(category = '') {
   let action = {}
   switch (category) {
     case 'something':
-      break;
+      break
     default:
       action = {
-        type: ['관리', '조회', '가공'],
-        fp: ['ILF', 'EQ', 'EO']
+        type: ['ASIS 정보조회', 'TOBE 정보입력'],
+        fp: ['EQ', 'EI'],
       }
-
+    // action = {
+    //   type: ['관리', '조회', '가공'],
+    //   fp: ['ILF', 'EQ', 'EO'],
+    // }
   }
 
   return action
 }
 
 let result = []
-const { type, fp } = getAction()
+const {type, fp} = getAction()
 input.forEach(ele1 => {
   type.forEach((ele2, i) => {
     let item = {}
     item.content = `${ele1} ${ele2}`
+    // console.log(ele2)
     item.type = fp[i]
     result.push(item)
   })
 })
 
-result.forEach(({ content, type }) => {
-  console.log(`${content}, ${type}`)
+let outstr = ''
+result.forEach(({content, type}) => {
+  const line = `${content}, , ${type} \n`
+  // console.log(line)
+  outstr = outstr.concat(line)
 })
+
+fs.writeFileSync('./output.csv', outstr, {encoding: 'utf8'})
